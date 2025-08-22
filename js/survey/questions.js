3c73f9cbed62b43d68b75dff30b8035f38245d9b
info <info@fairycat.biz>
2025-08-22 08:18:10 +0000

    modified:   index.html 	modified:   js/survey/questions.js 	modified:   survey.html

--- js/survey/questions.js
+++ js/survey/questions.js
@@ -10,23 +10,21 @@ const answers = [];
 export function start(json) {
   console.log("Questions.start()");
   const title = document.getElementById("surveyTitle");
-  const message = document.getElementById("surveyMessage");
   title.innerText = json.title;
-  message.innerHTML = "診断を開始します。";
   showQuestion(json.items, 0);
 }
 
 // 質問と回答を表示
 function showQuestion(items, num){
-  const item = items[0];
-  console.log(item);
 
+  // 設問データを取得
+  const item = items[num];
   const questionLabel = document.getElementById("surveyMessage");
-  questionLabel.innerText = `（${item.no} / ${items.length}）${item.label}`;
+  questionLabel.innerText = `（${item.no}/${items.length}）${item.label}`;
 
+  // 設問を表示
   const questionText = document.getElementById("questionText");
   questionText.innerText = item.question;
-
   const questionOptions = document.getElementById("questionOptions");
   questionOptions.innerHTML = [
     item.option1 ? createButton(1, item.option1) : '',
@@ -34,13 +32,17 @@ function showQuestion(items, num){
     item.option3 ? createButton(3, item.option3) : '',
     item.option4 ? createButton(4, item.option4) : '',
   ].join('');
+
+  // 非表示を解除
   const questionArea = document.getElementById("questionArea");
   questionArea.classList.remove('hidden');
+  const questionActions = document.getElementById("questionActions");
+  questionActions.classList.remove('hidden');
 }
 
 function createButton(num, caption){
   const class1 = 'block w-full py-3 px-4';
-  const class2 = 'text-xl font-semibold text-gray-700 peer-checked:bg-green-600 peer-checked:text-white';
+  const class2 = 'text-lg font-semibold text-gray-700 peer-checked:bg-green-600 peer-checked:text-white';
   const class3 = 'rounded-lg shadow-md bg-gray-200';
   const class4 = 'cursor-pointer transition-colors duration-200 relative';
     const button = [
